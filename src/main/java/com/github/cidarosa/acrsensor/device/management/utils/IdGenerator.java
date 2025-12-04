2065f328c63dc35566033ce80d226e38b1b498a2
cidarosa <lucita.rosa@gmail.com>
2025-12-03 23:45:37 -0300

    Add geradores de ID, UUID e TSID, testes

---
+++ src/main/java/com/github/cidarosa/acrsensor/device/management/utils/IdGenerator.java
@@ -0,0 +1,32 @@
+package com.github.cidarosa.acrsensor.device.management.utils;
+
+import io.hypersistence.tsid.TSID;
+
+import java.util.Optional;
+
+public class IdGenerator {
+
+    private static final TSID.Factory tsidFactory;
+
+    static {
+        //busca variável de ambiente das prorpiedades do sistema
+//        System.setProperty("tsid.node", "2");
+//        System.setProperty("tsid.node.count", "32"); //tamanho do cluster
+        //em produção setar as variáveis de ambiente, senão pega o padrão do TSID
+        Optional.ofNullable(System.getenv("tsid.node"))
+                .ifPresent(tsidNode -> System.setProperty("tsid.node", tsidNode));
+
+        Optional.ofNullable(System.getenv("tsid.node.count"))
+                .ifPresent(tsidNodeCount -> System.setProperty("tsid.node.count", tsidNodeCount));
+
+
+        tsidFactory = TSID.Factory.builder().build();
+    }
+
+    private IdGenerator() {
+    }
+
+    public static TSID generateTSID() {
+        return tsidFactory.generate();
+    }
+}
---
+++ src/test/java/com/github/cidarosa/acrsensor/device/management/TSIDTest.java
@@ -0,0 +1,29 @@
+package com.github.cidarosa.acrsensor.device.management;
+
+import com.github.cidarosa.acrsensor.device.management.utils.IdGenerator;
+import io.hypersistence.tsid.TSID;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
+
+class TSIDTest {
+
+    @Test
+    void shouldGenerateTSID() {
+
+        // Foi setado as variáveis de ambiente na IDE, node=7, node.count=64, em edit Configurations
+        TSID tsid = IdGenerator.generateTSID();
+
+        Assertions.assertThat(tsid.getInstant())
+                .isCloseTo(Instant.now(), Assertions.within(1, ChronoUnit.MINUTES));
+
+//        System.out.println("String: " + tsid);
+//        System.out.println(tsid.toLong());
+//        System.out.println(tsid.getInstant());
+
+
+    }
+
+}

148b71433b43eb7de7bfb09e8fe07b5136845e4d
cidarosa <lucita.rosa@gmail.com>
2025-12-09 16:54:37 -0300

    feat: erros de timeout

--- src/main/java/com/github/cidarosa/acrsensor/device/management/api/client/SensorMonitoringClientBadGatewayException.java
+++ src/main/java/com/github/cidarosa/acrsensor/device/management/api/client/SensorMonitoringClientBadGatewayException.java
@@ -1,9 +1,6 @@
 package com.github.cidarosa.acrsensor.device.management.api.client;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ResponseStatus;
-
-@ResponseStatus(HttpStatus.BAD_GATEWAY)
+//@ResponseStatus(HttpStatus.BAD_GATEWAY)
 public class SensorMonitoringClientBadGatewayException extends RuntimeException {
 
     //Custom exception - integration exception - status code 502
--- src/main/java/com/github/cidarosa/acrsensor/device/management/api/client/impl/SensorMonitoringClientImpl.java
+++ src/main/java/com/github/cidarosa/acrsensor/device/management/api/client/impl/SensorMonitoringClientImpl.java
@@ -4,8 +4,12 @@ import com.github.cidarosa.acrsensor.device.management.api.client.SensorMonitori
 import com.github.cidarosa.acrsensor.device.management.api.client.SensorMonitoringClientBadGatewayException;
 import io.hypersistence.tsid.TSID;
 import org.springframework.http.HttpStatusCode;
+import org.springframework.http.client.ClientHttpRequestFactory;
+import org.springframework.http.client.SimpleClientHttpRequestFactory;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestClient;
+
+import java.time.Duration;
 
 @Component
 public class SensorMonitoringClientImpl implements SensorMonitoringClient {
@@ -16,12 +20,23 @@ public class SensorMonitoringClientImpl implements SensorMonitoringClient {
 //com Builder, chama bind do Builder que o Spring já carregou, com config que o Spring adiciona
     public SensorMonitoringClientImpl(RestClient.Builder builder) {
         this.restClient = builder.baseUrl("http://localhost:8082")
+                //alterando config do client restclient padrão (simpleClient) para forçar tempo espera abaixo 10seg
+                .requestFactory(generateClientHttpRequestFactory())
                 //tratamento exceptions, qualquer tipo de erro
                 .defaultStatusHandler(HttpStatusCode::isError, (request, response) -> {
                     throw new SensorMonitoringClientBadGatewayException();
                 })
 
                 .build();
+    }
+
+    private ClientHttpRequestFactory generateClientHttpRequestFactory() {
+        SimpleClientHttpRequestFactory factory = new SimpleClientHttpRequestFactory();
+
+        factory.setReadTimeout(Duration.ofSeconds(5)); //espera da resposta
+        factory.setConnectTimeout(Duration.ofSeconds(3)); //espera para conseguir fazer conexão
+
+        return factory;
     }
 
     public void enableMonitoring(TSID sensorId) {
